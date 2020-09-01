007133a608c8d70b412e46d802aafa9fcb6ab083
mfouchi <mfouchi@gmail.com>
2020-09-01 09:42:46 -0500

    Resume updates

--- php/send_mail.php
+++ php/send_mail.php
@@ -1,4 +1,5 @@
 <?php
+require_once "Mail.php";
 $name = $_POST['name'];
 $email = $_POST['email'];
 $comment = $_POST['comment'];
@@ -15,11 +16,33 @@ if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
 	$message = 'Hello,<br/>'
 	. 'Name:' . $name . '<br/>'	
 	. 'Message:' . $comment . '<br/><br/>';		
-	mail($toEmail, $subject, $message, $headers);
-	$showMessage = "Thank you.  Your message has been received. I will contact you soon.";	
+	
+	//$this_mail = mail($toEmail, $subject, $message, $headers);
+  
+
+$host = "mail.pixelclickllc.com";
+$port = 26; // NOTICE THIS PORT HAS TO BE 26, NOT 25
+$username = ""; //YES, THAT IS  A “PLUS” SIGN
+$password = "";
+
+$headers = array ('From' => $name + '<' + $email + '>',
+'To' => $toEmail,
+'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+array ('host' => $host,
+'port' =>$port,
+'auth' => true,
+'username' => $username,
+'password' => $password));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+	$showMessage = $mail->getMessage();
 } else {
-	$showMessage =  "invalid email";
+	$showMessage = "Message successfully sent!";
 }
+
 $jsonData = array(
 	"message"	=> $showMessage
 );
